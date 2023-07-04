fed276ca6f5beb5862d5e90869dc883b9de6125f
pranish <pranishshrestha181@gmail.com>
2023-07-04 10:59:10 +0545

    feat: initial commit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -31,7 +31,7 @@ export const Search = ({ books }) => {
       <div className="search-container">
         <input
           type="text"
-          placeholder="Search ..."
+          placeholder="Search by book name..."
           value={query}
           className="search"
           onChange={handleSearch}
@@ -39,22 +39,28 @@ export const Search = ({ books }) => {
       </div>
       <div>
       <div className="item-container">
-        {searchResults.map(({ image, id,price, title, author, category }) => (
-          <div key={id} className="item">
-            <h3>{title}</h3>
-            <p>Author: {author}</p>
-            <img src={image} alt={title} />
-            <p>{price}</p>
-            <select
-              value={category}
-              onChange={(e) => handleAddToLibrary(e, title)}
-            >
-              <option value="currently reading">Currently Reading</option>
-              <option value="want to read">Want to Read</option>
-              <option value="read">Read</option>
-            </select>
-          </div>
-        ))}
+        {searchResults.length === 0 ? (
+          <p>Please Search Something</p>
+        ):
+        (
+          searchResults.map(({ image, id,price, title, author, category }) => (
+            <div key={id} className="item">
+              <h3>{title}</h3>
+              <p>Author: {author}</p>
+              <img src={image} alt={title} />
+              <p>{price}</p>
+              <select
+                value={category}
+                onChange={(e) => handleAddToLibrary(e, title)}
+              >
+                <option value="currently reading">Currently Reading</option>
+                <option value="want to read">Want to Read</option>
+                <option value="read">Read</option>
+              </select>
+            </div>
+          ))
+        )}
+      
         </div>
       </div>
     </>
